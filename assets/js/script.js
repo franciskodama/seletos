b448a9f01035765cc805defcf68bb98387afa5bd
Kodama <fk@fkodama.com>
2021-04-13 17:57:28 -0300

    Create nav bar desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,32 @@
-// Buttons | Smooth navigation + Unhide content behind (position - 70px) -----------
-
-const links = document.querySelectorAll(".js-button");
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop - 70;
-  console.log(offsetTop);
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
-}
-
-
-
-// Mobile Hamburguer Menu | Open and Close ---------------------------------------
+// MOBILE HAMBURGUER MENU | Open and Close ---------------------------------------
 
 const closeButton = document.querySelector('.close-nav');
 const openButton = document.querySelector('.open-nav');
 const nav = document.querySelector('nav');
+const buttonHamburguer = document.querySelector('.button-hamburguer');
 
 closeButton.addEventListener("click", () => {
   nav.classList.remove('navigation-open');
+  buttonHamburguer.classList.remove('button-hamburguer-hidden');
 });
 
 openButton.addEventListener("click", () => {
   nav.classList.add('navigation-open');
+  buttonHamburguer.classList.add('button-hamburguer-hidden');
 });
 
-const closeButtonAfterClick = document.querySelectorAll('.js-button');
-for (var i = 0; i < closeButtonAfterClick.length; i++) {
+const closeButtonAfterClick = document.querySelectorAll('.smooth-scroll');
+for (let i = 0; i < closeButtonAfterClick.length; i++) {
   closeButtonAfterClick[i].addEventListener("click", () => {
     nav.classList.remove('navigation-open');
+    buttonHamburguer.classList.remove('button-hamburguer-hidden');
   });
 }
 
 
-// Packages Slider ---------------------------------------
+// CAROUSEL: PACKAGES AND TESTIMONIALS ---------------------------------------
 
-new Glide('.glide', {
+new Glide('.carousel-packages', {
   type: 'carousel',
   perView: 3,
   autoplay: 3000,
@@ -59,13 +42,25 @@ new Glide('.glide', {
 
 }).mount()
 
+new Glide('.carousel-testimonials', {
+  type: 'carousel',
+  perView: 1,
+  autoplay: 3000,
+
+}).mount()
 
 
+// TO TOP BUTTON ---------------------------------------
 
-//SELECTORS --------------------------------
+window.onscroll = function () {
+    if (pageYOffset >= 200) {
+        document.getElementById('backToTop').style.visibility = "visible";
+    } else {
+ document.getElementById('backToTop').style.visibility = "hidden";
+    }
+};
 
-
-//EVENT LISTENERS --------------------------
-
-
-//FUNCTIONS --------------------------------
+document.getElementById('backToTop').onclick = function()
+{
+    scrollTo(document.body, 0, 0);
+}
