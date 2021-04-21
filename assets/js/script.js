806a12d053d00ee8043a572ba5b48346da687d95
Kodama <fk@fkodama.com>
2021-04-21 13:32:23 -0300

    Modify 2 columns for specialities area

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,90 @@ document.getElementById('backToTop').onclick = function()
 {
     scrollTo(document.body, 0, 0);
 }
+
+
+// ANIMATIONS ---------------------------------------
+
+const animateOnScroll = function ({ offset } = { offset: 10 }) {
+  
+  // Define a dobra superior, inferior e laterais da tela
+  const windowTop = (offset * window.innerHeight) / 100;
+  const windowBottom = window.innerHeight - windowTop;
+  const windowLeft = 0;
+  const windowRight = window.innerWidth;
+
+  this.start = (element) => {
+    window.requestAnimationFrame(() => {
+      // Seta os atributos customizados
+      element.style.animationDelay = element.dataset.animationDelay;
+      element.style.animationDuration = element.dataset.animationDuration;
+
+      // Inicia a animacao setando a classe para animar
+      element.classList.add(element.dataset.animation);
+
+      // Seta o elemento como animado
+      element.dataset.animated = "true";
+    });
+  };
+
+  this.inViewport = (element) => {
+    // Obtem o boundingbox do elemento
+    const elementRect = element.getBoundingClientRect();
+    const elementTop =
+      elementRect.top + parseInt(element.dataset.animationOffset) ||
+      elementRect.top;
+    const elementBottom =
+      elementRect.bottom - parseInt(element.dataset.animationOffset) ||
+      elementRect.bottom;
+    const elementLeft = elementRect.left;
+    const elementRight = elementRect.right;
+
+    // Verifica se o elemento esta na tela
+    return (
+      elementTop <= windowBottom &&
+      elementBottom >= windowTop &&
+      elementLeft <= windowRight &&
+      elementRight >= windowLeft
+    );
+  };
+
+  // Percorre o array de elementos, verifica se o elemento está na tela e inicia animação
+  this.verifyElementsInViewport = (els = elements) => {
+    for (let i = 0, len = els.length; i < len; i++) {
+      // Passa para o proximo laço se o elemento ja estiver animado
+      if (els[i].dataset.animated) continue;
+
+      this.inViewport(els[i]) && this.start(els[i]);
+    }
+  };
+
+  // Obtem todos os elementos a serem animados
+  this.getElements = () =>
+    document.querySelectorAll("[data-animation]:not([data-animated])");
+
+  // Atualiza a lista de elementos a serem animados
+  this.update = () => {
+    elements = this.getElements();
+    elements && this.verifyElementsInViewport(elements);
+  };
+
+  // Inicia os eventos
+  window.addEventListener("load", this.update, false);
+  window.addEventListener(
+    "scroll",
+    () => this.verifyElementsInViewport(elements),
+    { passive: true }
+  );
+  window.addEventListener(
+    "resize",
+    () => this.verifyElementsInViewport(elements),
+    { passive: true }
+  );
+};
+
+// Initialize
+const options = {
+  offset: 15 // percentage of the window
+};
+
+const animation = new animateOnScroll(options);
